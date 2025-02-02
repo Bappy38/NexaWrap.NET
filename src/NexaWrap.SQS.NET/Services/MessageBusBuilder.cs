83e995ad0747b95ce7fdcff8895e2be20735e260
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-02 13:03:00 +0600

    MessageBusBuilder defined to hold messages and their respective handlers

---
+++ src/NexaWrap.SQS.NET/Services/MessageBusBuilder.cs
@@ -0,0 +1,22 @@
+﻿using NexaWrap.SQS.NET.Interfaces;
+using NexaWrap.SQS.NET.Models;
+
+namespace NexaWrap.SQS.NET.Services;
+
+public class MessageBusBuilder
+{
+    public Dictionary<Type, Type> HandlersDict { get; set; }
+
+    public MessageBusBuilder()
+    {
+        HandlersDict = new Dictionary<Type, Type>();
+    }
+
+    public MessageBusBuilder RegisterHandler<TMessage, THandler>()
+        where TMessage : class, IMessage
+        where THandler : class, IMessageHandler<TMessage>
+    {
+        HandlersDict.Add(typeof(TMessage), typeof(THandler));
+        return this;
+    }
+}
