fa2bc90536c191765e716d5dbda12e60a260f9aa
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-02 13:00:51 +0600

    Necessary constants defined

---
+++ src/NexaWrap.SQS.NET/Constants/MessageAttributes.cs
@@ -0,0 +1,7 @@
+﻿namespace NexaWrap.SQS.NET.Constants;
+
+public class MessageAttributes
+{
+    public const string MessageTypeName = "MessageTypeName";
+    public const string CorrelationId = "CorrelationId";
+}
