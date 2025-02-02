e40b9dd0805179d83a39a903241a266467614fe6
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-02 13:02:26 +0600

    dispatcher and consumer implemented

---
+++ src/NexaWrap.SQS.NET/Services/MessageConsumer.cs
@@ -0,0 +1,101 @@
+﻿using Amazon.SQS.Model;
+using Amazon.SQS;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NexaWrap.SQS.NET.Models;
+using System.Net;
+using System.Text.Json;
+
+namespace NexaWrap.SQS.NET.Services;
+
+public class MessageConsumer : BackgroundService
+{
+    private readonly IAmazonSQS _sqsClient;
+    private readonly MessageDispatcher _messageDispatcher;
+    private readonly ILogger<MessageConsumer> _logger;
+    private readonly SqsOptions _sqsOptions;
+
+    private readonly List<string> _messageAttributeNames = new List<string> { "All" };
+
+    public MessageConsumer(
+        IAmazonSQS sqsClient,
+        MessageDispatcher messageDispatcher,
+        ILogger<MessageConsumer> logger,
+        IOptions<SqsOptions> sqsOptions)
+    {
+        _sqsClient = sqsClient;
+        _messageDispatcher = messageDispatcher;
+        _logger = logger;
+        _sqsOptions = sqsOptions.Value;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var queueUrl = await _sqsClient.GetQueueUrlAsync(_sqsOptions.SubscribedQueueName, stoppingToken);
+
+        var receiveRequest = new ReceiveMessageRequest
+        {
+            QueueUrl = queueUrl.QueueUrl,
+            MessageAttributeNames = _messageAttributeNames,
+            MessageSystemAttributeNames = _messageAttributeNames,
+            MaxNumberOfMessages = _sqsOptions.MaxBatchSize,
+            WaitTimeSeconds = _sqsOptions.WaitTimeSeconds
+        };
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var messageResponse = await _sqsClient.ReceiveMessageAsync(receiveRequest, stoppingToken);
+
+            if (messageResponse.HttpStatusCode != HttpStatusCode.OK)
+            {
+                _logger.LogError("Receive message request from SQS queue with name {SubscribedQueueName} failed with ResponseMetadata {ResponseMetadata}", _sqsOptions.SubscribedQueueName, messageResponse.ResponseMetadata);
+                continue;
+            }
+
+            await ProcessMessagesAsync(messageResponse.Messages, queueUrl.QueueUrl, stoppingToken);
+        }
+    }
+
+    private async Task ProcessMessagesAsync(List<Message> messages, string queueUrl, CancellationToken cancellationToken)
+    {
+        var tasks = new List<Task>();
+        foreach (var message in messages)
+        {
+            tasks.Add(ProcessMessageAsync(message, queueUrl, cancellationToken));
+        }
+        await Task.WhenAll(tasks);
+    }
+
+    private async Task ProcessMessageAsync(Message message, string queueUrl, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var messageBody = message.Body;
+            var messageTypeName = message.MessageAttributes.GetValueOrDefault(nameof(IMessage.MessageTypeName))?.StringValue;
+
+            if (string.IsNullOrEmpty(messageTypeName))
+            {
+                _logger.LogError("MessageConsumer received a message with null or empty MessageTypeName");
+                return;
+            }
+
+            if (!_messageDispatcher.CanHandleMessageType(messageTypeName))
+            {
+                _logger.LogInformation("Didn't find any handler to handle the message with type name {messageTypeName}", messageTypeName);
+                return;
+            }
+
+            var messageType = _messageDispatcher.GetMessageTypeByName(messageTypeName)!;
+            var deserializedMessage = (IMessage)JsonSerializer.Deserialize(messageBody, messageType)!;
+
+            await _messageDispatcher.DispatchAsync(deserializedMessage);
+
+            await _sqsClient.DeleteMessageAsync(queueUrl, message.ReceiptHandle, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+        }
+    }
+}
---
+++ src/NexaWrap.SQS.NET/Services/MessageDispatcher.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NexaWrap.SQS.NET.Interfaces;
+using NexaWrap.SQS.NET.Models;
+
+namespace NexaWrap.SQS.NET.Services;
+
+public class MessageDispatcher
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    private readonly Dictionary<string, Type> _messageTypeMappings = new();
+    private readonly Dictionary<string, Type> _handlerTypeMappings = new();
+
+    public MessageDispatcher(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    internal void RegisterHandler(Type messageType, Type handlerType)
+    {
+        var messageTypeName = messageType.Name;
+
+        _messageTypeMappings.Add(messageTypeName, messageType);
+        _handlerTypeMappings.Add(messageTypeName, handlerType);
+    }
+
+    public async Task DispatchAsync(object message)
+    {
+        var messageType = message.GetType();
+        var messageTypeName = messageType.Name;
+        _handlerTypeMappings.TryGetValue(messageTypeName, out var handlerType);
+
+        if (handlerType == null)
+        {
+            throw new Exception("Handler type not found");
+        }
+
+        using var scope = _scopeFactory.CreateScope();
+
+        var handler = scope.ServiceProvider.GetService(handlerType);
+
+        if (handler is null)
+        {
+            throw new Exception($"Handler for message type {messageTypeName} not found");
+        }
+
+        var handleMethodName = nameof(IMessageHandler<IMessage>.HandleAsync);
+
+        var handleMethod = handlerType.GetMethod(handleMethodName);
+
+        if (handleMethod is null)
+        {
+            throw new Exception($"Handle method {handleMethodName} not found");
+        }
+
+        await (Task)handleMethod.Invoke(handler, [message]);
+    }
+
+    public bool CanHandleMessageType(string messageTypeName)
+    {
+        return _handlerTypeMappings.ContainsKey(messageTypeName);
+    }
+
+    public Type? GetMessageTypeByName(string messageTypeName)
+    {
+        return _messageTypeMappings.GetValueOrDefault(messageTypeName);
+    }
+}
+
